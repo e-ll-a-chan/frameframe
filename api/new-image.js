3c2f0f8b6f8ab69324961a12c5c1e853d04c8770
ella_chan_ <86010126+e-ll-a-chan@users.noreply.github.com>
2024-07-27 23:59:02 +0900

    Update new-image.js

--- api/new-image.js
+++ api/new-image.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     <head>
       <meta charset="UTF-8">
       <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://picsum.photos/1200/630?random=${randomParam}" />
+      <meta property="fc:frame:image" content="https://raw.githubusercontent.com/e-ll-a-chan/frameframe/master/second.png" />
       <meta property="fc:frame:button:1" content="Retry" />
       <meta property="fc:frame:post_url" content="https://0kcal-game.vercel.app/api/new-image" />
       <title>New Image - Farcaster Frame</title>
